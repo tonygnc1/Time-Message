77e91f1c39d69e69f932e841a2ecdfc6849f82c4
Tony <bootcamp1955@gmail.com>
2019-11-22 17:02:44 -0500

    App.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+button.addEventListener('click',showOutput);
+
+function showOutput() {
+    const date = new Date();
+    let cur = (date.getHours());
+    let message;
+
+    if(cur>17){
+        message = 'Its evening';
+    } else if(cur>12) {
+        message = 'Its afternoon';
+    } else if(cur<8) {
+message = "We should be in bed";
+    } else {
+        message = 'Something is wrong';
+    }
+    output.innerHTML = '<h1>' + message + '</h1>';
+}
